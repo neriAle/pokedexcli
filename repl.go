a248b662a64beb8939ad2ff9a5e3fc6ca9fd2833
Alessandro Neri <a.neri@student.tudelft.nl>
2025-07-29 16:15:05 +0200

    ✨ Added commands struct and 'exit' command callback

--- repl.go
+++ repl.go
@@ -7,6 +7,20 @@ import(
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand = map[string]cliCommand{
+    "exit": {
+        name:        "exit",
+        description: "Exit the Pokedex",
+        callback:    commandExit,
+    },
+}
+
 func startREPL() {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
@@ -14,10 +28,27 @@ func startREPL() {
 		reader.Scan()
 
 		cleanText := cleanInput(reader.Text())
-		if len(cleanText) > 0 {
-			fmt.Printf("Your command was: %s\n", cleanText[0])
+		if len(cleanText) < 1 {
+			continue
+		}
+
+		cmd, ok := commands[cleanText[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
 		}
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
 
 func cleanInput(text string) []string {
