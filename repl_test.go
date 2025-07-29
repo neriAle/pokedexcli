2ae9734cd3dc6f2fe438b8ccb3271fe1a154761f
Alessandro Neri <a.neri@student.tudelft.nl>
2025-07-29 14:38:29 +0200

    ✅ Tested cleanInput method

---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import(
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+			input: " Hello world   ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "SOME things NeVeR chanGE",
+			expected: []string{"some", "things", "never", "change"},
+		},
+		{
+			input: "   ",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("size of slice is incorrect")
+		} else {
+			for i, _ := range actual {
+				word := actual[i]
+				exWord := c.expected[i]
+				if word != exWord {
+					t.Errorf("'%s' found, but expected '%s'", word, exWord)
+					break
+				}
+ 			}
+		}
+	}
+}
