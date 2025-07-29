745ee149c64441b50799d835b10b305a1c08baa5
Alessandro Neri <a.neri@student.tudelft.nl>
2025-07-29 15:23:10 +0200

    ✨ Implemented basic REPL

--- repl.go
+++ repl.go
@@ -1,8 +1,24 @@
 package main
 
 import(
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startREPL() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		cleanText := cleanInput(reader.Text())
+		if len(cleanText) > 0 {
+			fmt.Printf("Your command was: %s\n", cleanText[0])
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	lowercaseText := strings.ToLower(text)
